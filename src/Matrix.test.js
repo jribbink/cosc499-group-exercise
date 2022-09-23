723035227d9b0af2ab02aacd7f7ba2cdba67e1d5
Joonsik Kim <60025350+prokjs@users.noreply.github.com>
2022-09-22 20:38:23 -0700

    Added add matrix feature (#10)

--- src/Matrix.test.js
+++ src/Matrix.test.js
@@ -5,6 +5,7 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import { act, fireEvent } from "@testing-library/react";
 import Matrix from "./Matrix";
+import { addMatrix } from "./calculate";
 
 describe("Matrix tests", () => {
   let container, root;
@@ -64,4 +65,24 @@ describe("Matrix tests", () => {
 
     expect(newValue[i][j]).toEqual(v);
   });
+
+  test("addition of values", async() => {
+    let add1 = [
+      [1, 2, 3],
+      [4, 5, 6],
+      [7, 8, 9],
+    ];
+    let add2 = [
+      [9, 8, 7],
+      [6, 5, 4],
+      [3, 2, 1],
+    ];
+    let add3 = addMatrix(add1,add2);
+    for(let i = 0; i < 3; i++){
+      for(let j = 0; j < 3; j++){
+        expect(add3[i][j]).toEqual(10);
+      }
+    }
+  });
+
 });
--- src/calculate.js
+++ src/calculate.js
@@ -1,5 +1,17 @@
 export function addMatrix(a, b) {
-  return null;
+  if (a.length !== b.length || a[0].length !== b[0].length) {
+    throw new Error("Matrices are different dimensions");
+  }
+
+  let c = [];
+  for (let i = 0; i < a.length; i++) {
+    let r = [];
+    for (let j = 0; j < a[0].length; j++) {
+      r.push(a[i][j] + b[i][j]);
+    }
+    c.push(r);
+  }
+  return c;
 }
 
 export function subtractMatrix(a, b) {
